54004b930ae671934d77b7839d86010e95d5fec6
kelvin <kelvinmutugi336@gmil.com>
2024-05-27 12:13:05 +0300

    solved lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "its me";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "thats wrong";
+}
